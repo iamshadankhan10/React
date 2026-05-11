ab1102287e666bc4e80148c033c3092c837d4f31
Shadan Raza Khan <iamshadankhan10@gmail.com>
2026-05-12 02:36:31 +0530

    Implement user input handling and display in the React app

--- two-way-binding/src/App.jsx
+++ two-way-binding/src/App.jsx
@@ -1,21 +1,50 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const App = () => {
 
-  const submitHandler = (e) =>{
-    e.preventDefault();
-    console.log("Form submitted");
+  const [title, setTitle] = useState('')
+  const [allUsers, setAllUsers] = useState([])
+
+  const submitHandler =(e) =>{
+    e.preventDefault()
+
+    const oldUsers = [...allUsers]
+    oldUsers.push(title)
+    console.log(oldUsers);
+
+    setAllUsers(oldUsers)
     
+
+    const newAllUsers = [...allUsers]
+    newAllUsers.push(title)
+    console.log(newAllUsers);
+    
+
+    setTitle('')
   }
 
   return (
     <div>
-      <form onSubmit={(e) => {
+      <form onSubmit={(e)=>{
         submitHandler(e)
       }}>
-        <input type="text" placeholder="Enter your name" />
+        <input
+        type="text"
+        placeholder='Enter the name'
+        value={title}
+        required
+        onChange={(e)=>{
+
+          setTitle(e.target.value);
+
+        }}/>
         <button type="submit">Submit</button>
       </form>
+
+    {allUsers.map((elem,idx)=>{
+      return <h1 key={idx}>{elem}</h1>
+    })}
+
     </div>
   )
 }
