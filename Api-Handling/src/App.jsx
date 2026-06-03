e9f68601777c21eb0c4e8c096f67bdbb26871e47
Shadan Raza Khan <iamshadankhan10@gmail.com>
2026-06-04 03:46:17 +0530

    Add User component for displaying user information with random background colors

--- Api-Handling/src/App.jsx
+++ Api-Handling/src/App.jsx
@@ -1,34 +1,30 @@
+import React from 'react'
+import axios from 'axios'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import User from './components/User'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+  const [allData, setAllData] = useState([])
+  const getData = async () => {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/users")
+    setAllData(response.data);
+    console.log(response.data);
+    
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <button onClick={getData}>Get Data</button>
+
+      <div className='all-cards'>
+        {allData.map(function(elem, idx){
+
+        return <div key={idx}>
+          <User elem={elem} />
+        </div>
+      })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
